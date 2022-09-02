6d40eed355e6907f4e8f6c96d97e9275c4861c1d
yd <vtedeweb@163.com>
2022-09-02 14:03:17 +0800

    init

---
+++ Say/MySay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Say
+{
+    public class MySay
+    {
+        public void SayHello(string name)
+        {
+            Trace.WriteLine($"say hello to {name}");
+        }
+    }
+}
